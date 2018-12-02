aa727516c614cc6ffab51f23e87e25483e68f1f0
Jason Jang <jwj1821@gmail.com>
2018-12-02 17:23:53 -0500

    fixed some codes in main.js

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+members = [
+  "Dave Grohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett"
+
+]
+
+tour = {
+  Chicago : "July 29th & 30th, 2018",
+  Seattle : "September 1st, 2018",
+  Edmonton : "September 4th, 2018"
+}
+
+
+
+var x = 0;
+
+while (x <5) {
+  console.log("Times running through the loop %s",x)
+  x++;
+}
