dcdd0133f2adad9b7112c1c86379c64db0bfbcbe
Jason Jang <jwj1821@gmail.com>
2018-12-02 03:27:30 -0500

    changes for branch javascript

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+members = [
+  "Dave Grohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett"
+
+]
+
+tour = {
+  Chicago : "July 29th & 30th, 2018",
+  Seattle : "September 1st, 2018",
+  Edmonton : "September 4th, 2018"
+}
+
+var x = 0;
+
+while (x <5) {
+  document.getElementById("Times running through the loop %d",x)
+  x++;
+}
